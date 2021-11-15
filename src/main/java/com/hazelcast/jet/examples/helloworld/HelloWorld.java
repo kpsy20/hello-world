d5e0df6e2a4615aa30a90f522bbca9a5301a8046
kpsy20 <kpsy20@naver.com>
2021-11-16 00:50:51 +0900

    private make

--- src/main/java/com/hazelcast/jet/examples/helloworld/HelloWorld.java
+++ src/main/java/com/hazelcast/jet/examples/helloworld/HelloWorld.java
@@ -3,14 +3,15 @@ package com.hazelcast.jet.examples.helloworld;
 import cp.swig.*;
 
 public class HelloWorld {
+    private static cloud_profilerJNI cpj = new cloud_profilerJNI();
     public static void main(String[] args) {
         try {
             Class.forName("com.hazelcast.jet.examples.helloworld.CloudProfiler");
         } catch (Exception e) {
             System.out.println(e);
         }
-        long ch = cp.swig.cloud_profiler.openChannel("test", cp.swig.log_format.ASCII, cp.swig.handler_type.IDENTITY);
-        cp.swig.cloud_profiler.logTS(ch, 0);
+        long ch = cloud_profiler.openChannel("test", log_format.ASCII, handler_type.IDENTITY);
+        cloud_profiler.logTS(ch, 0);
     }
 }
 
