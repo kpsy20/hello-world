bb701847efdb271b1d5fab5e7d1ad0fec74991ea
kpsy20 <kpsy20@naver.com>
2021-11-20 10:38:46 +0000

    simple to fix

--- src/main/java/com/hazelcast/jet/examples/helloworld/CloudProfiler.java
+++ src/main/java/com/hazelcast/jet/examples/helloworld/CloudProfiler.java
@@ -5,6 +5,19 @@ public class CloudProfiler {
     static{
         try {
             System.load("/usr/lib/libcloud_profiler.so");
+	   
+	    System.load("/opt/squash/0.8/lib/libsquash0.8.so.0.8.0");
+	    System.load("/home/namsick96_yonsei_ac_kr/cloud_profiler/build_rel/src/cp/config_server/libnet_conf.so");
+	    System.load("/opt/boost/1.76.0/lib/libboost_system.so.1.76.0");
+	    System.load("/opt/zmq/4.3.4/lib/libzmq.so.5");
+	    System.load("/lib64/libstdc++.so.6");
+	    System.load("/lib64/libm.so.6");
+	    System.load("/lib64/libgcc_s.so.1");
+	    System.load("/lib64/libc.so.6");
+	    System.load("/lib64/libpthread.so.0");
+	    System.load("/lib64/libdl.so.2");
+	    System.load("/lib64/librt.so.1");
+	    System.load("/lib64/ld-linux-x86-64.so.2");
 	//    System.load("/usr/lib/libnet_conf.so");
 	//    System.loadLibrary("cloud_profiler");
         }
--- src/main/java/com/hazelcast/jet/examples/helloworld/HelloWorld.java
+++ src/main/java/com/hazelcast/jet/examples/helloworld/HelloWorld.java
@@ -1,4 +1,17 @@
 package com.hazelcast.jet.examples.helloworld;
+import com.hazelcast.function.ComparatorEx;
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.Observable;
+import com.hazelcast.jet.aggregate.AggregateOperations;
+import com.hazelcast.jet.config.JobConfig;
+import com.hazelcast.jet.config.ProcessingGuarantee;
+import com.hazelcast.jet.datamodel.WindowResult;
+import com.hazelcast.jet.function.Observer;
+import com.hazelcast.jet.pipeline.*;
+import com.hazelcast.jet.pipeline.file.FileSources;
+import com.hazelcast.jet.pipeline.test.TestSources;
+//import com.hazelcast.jet.examples.helloworld.CustomSources;
 
 import cp.swig.*;
 import cp.swig.log_format;
@@ -6,6 +19,7 @@ import cp.swig.handler_type;
 public class HelloWorld {
     public static void main(String[] args) {
         try {
+//	ClassLoaderUtil.newInstance(null, "com.hazelcast.jet.examples.helloworld.CloudProfiler");
             Class.forName("com.hazelcast.jet.examples.helloworld.CloudProfiler");
         } catch (Exception e) {
             System.out.println(e);
